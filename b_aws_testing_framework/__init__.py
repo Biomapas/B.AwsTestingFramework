c6e5be64a707237c8256c18d281eafe05f77e4b2
laimonassutkus <laimonas.sutkus@gmail.com>
2021-03-30 16:38:01 +0300

    Fix path.

--- b_aws_testing_framework/__init__.py
+++ b_aws_testing_framework/__init__.py
@@ -1,3 +1,5 @@
-with open('./../VERSION') as file:
+from os.path import abspath, dirname
+
+with open(f'{abspath(dirname(__file__))}/../VERSION') as file:
     b_aws_testing_framework_version = file.read()
     b_aws_testing_framework_version = ''.join(b_aws_testing_framework_version.split())
